657c74a6d0df5aa53427971adf77483208fc8899
Leonardo Machado <leonardomachado200112@gmail.com>
2022-11-09 23:16:48 -0300

    ending of chapter iv

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -7,6 +7,8 @@ import { SpecificationsRepository } from "../../modules/cars/infra/repositories/
 import { UsersRepository } from "../../modules/accounts/infra/repositories/UsersRepository";
 import { ICarsRepository } from "../../modules/cars/repositories/ICarsRepository";
 import { CarsRepository } from "../../modules/cars/infra/repositories/CarsRepository";
+import { ICarsImagesRepository } from "../../modules/cars/repositories/ICarsImagesRepository";
+import { CarsImagesRepository } from "../../modules/cars/infra/repositories/CarsImagesRepository";
 
 container.registerSingleton<ICategoriesRepository>(
     "CategoriesRepository",
@@ -26,4 +28,9 @@ container.registerSingleton<IUsersRepository>(
 container.registerSingleton<ICarsRepository>(
     "CarsRepository",
     CarsRepository
+)
+
+container.registerSingleton<ICarsImagesRepository>(
+    "CarsImagesRepository",
+    CarsImagesRepository
 )
