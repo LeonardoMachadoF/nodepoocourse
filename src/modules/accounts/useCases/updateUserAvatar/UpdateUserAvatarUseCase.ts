95b3a174688bff42e4ec4eeee4fc028ac1e3fd50
Leonardo Machado Fabris <leonardomachado200112@gmail.com>
2022-11-19 07:19:59 -0300

    aws

--- src/modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarUseCase.ts
+++ src/modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarUseCase.ts
@@ -1,4 +1,5 @@
 import { inject, injectable } from "tsyringe";
+import { IStorageProvider } from "../../../../shared/container/providers/StorageProvider/IStorageProvider";
 import { deleteFile } from "../../../../utils/file";
 import { User } from "../../infra/typeorm/entities/User";
 import { IUsersRepository } from "../../repositories/IUsersRepository";
@@ -12,14 +13,18 @@ interface IRequest {
 export class UpdateUserAvatarUseCase {
     constructor(
         @inject('UsersRepository')
-        private usersRepository: IUsersRepository
+        private usersRepository: IUsersRepository,
+        @inject("StorageProvider")
+        private storageProvider: IStorageProvider
     ) { }
     async execute({ user_id, avatarFile }: IRequest): Promise<void> {
         const user = await this.usersRepository.findById(user_id);
 
         if (user && user.avatar) {
-            await deleteFile(`./tmp/avatar/${user.avatar}`);
+            await this.storageProvider.delete(user.avatar, 'avatar');
         }
+
+        await this.storageProvider.save(avatarFile, 'avatar')
         user!.avatar = avatarFile;
 
         await this.usersRepository.create(user as User);
--- src/shared/infra/http/routes/users.routes.ts
+++ src/shared/infra/http/routes/users.routes.ts
@@ -8,7 +8,7 @@ import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
 
 const usersRouter = Router();
 
-const uploadAvatar = multer(uploadConfig.upload('./tmp/avatar'));
+const uploadAvatar = multer(uploadConfig);
 
 const getUsersController = new GetUsersController();
 const createUserController = new CreateUserController();
